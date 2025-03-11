c694947a9d75ec5174cc09c73efe257299c8106d
Maris <maristel.lluch@student.ateneo.edu>
2025-03-11 08:06:04 +0800

    removed files

--- FinalProject-AsisGregorioLluch/FINAL na FINAL Lazapee/Lazapee/payroll_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Employee, Payslip
-# Register your models here.
-
-admin.site.register(Employee)
-admin.site.register(Payslip)
--- finalprojectV4/libapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LibappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'libapp'
